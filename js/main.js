91f874c9b0c7bdd73b19589249ba47f676d3cded
GogeH <skibadee@inbox.ru>
2020-07-04 18:06:40 +0300

    Личный проект: доверяй, но проверяй (часть 2)(4.3)

--- js/main.js
+++ js/main.js
@@ -161,7 +161,7 @@ function createAdAndAddToDOM(obj) {
   var popupAvatar = objNode.querySelector('.popup__avatar');
 
   // Функция выбора варианта для отображения типа жилья
-  function defineTypeHouse() {
+  function defineTypeHouse(homeType) {
 
     switch (homeType) {
       case 'flat':
